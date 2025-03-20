e0abe51293c38952a25044a42250291dc51d5477
felipprodrigues <dev.rodriguesfelipp@gmail.com>
2025-03-20 10:01:51 -0300

    refine styling retry and  & download functionality

--- components/upload-widget-upload-list.tsx
+++ components/upload-widget-upload-list.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useUploads } from "../store/uploads";
 import { UploadWidgetUploadItem } from "./upload-widget-upload-item";
+import * as ScrollArea from "@radix-ui/react-scroll-area"
 
 export function UploadWidgetUploadList(){
   const uploads = useUploads(store => store.uploads)
@@ -12,15 +13,26 @@ export function UploadWidgetUploadList(){
         <span className="text-zinc-400">({uploads.size})</span>
       </span>
 
-      {isUploadListEmpty ? (
-        <span className="text-xs text-zinc-400">No uploads added</span>
-      ) : (
-        <div className="flex flex-col gap-2">
-          {Array.from(uploads.entries()).map(([uploadId, upload]) => {
-            return <UploadWidgetUploadItem key={uploadId} upload={upload} uploadId={uploadId}/>
-          })}
-        </div>
-      )}
+      <ScrollArea.Root className="overflow-hidden" type="scroll">
+        <ScrollArea.Viewport className="h-[220px]">
+          {isUploadListEmpty ? (
+            <span className="text-xs text-zinc-400">No uploads added</span>
+          ) : (
+            <div className="flex flex-col gap-2">
+              {Array.from(uploads.entries()).map(([uploadId, upload]) => {
+                return <UploadWidgetUploadItem key={uploadId} upload={upload} uploadId={uploadId}/>
+              })}
+            </div>
+          )}
+        </ScrollArea.Viewport>
+
+        <ScrollArea.Scrollbar
+          className="ScrollAreaScrollbar"
+          orientation="vertical"
+        >
+          <ScrollArea.Thumb className="ScrollAreaThumb" />
+        </ScrollArea.Scrollbar>
+      </ScrollArea.Root>
     </div>
   )
 }
